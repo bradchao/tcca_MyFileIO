f285c5c4dba94149649857b4f57bb22cf7e6069f
Brad <brad@brad.tw>
2017-08-04 21:03:43 +0800

    2017080403

--- app/src/main/java/tw/brad/android/games/myfileio/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/myfileio/MainActivity.java
@@ -1,19 +1,35 @@
 package tw.brad.android.games.myfileio;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    private SharedPreferences sp;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.tv);
+
+        sp = getSharedPreferences("mydata", MODE_PRIVATE);
+        editor = sp.edit();
+
     }
 
     public void test1(View view){
-
+        editor.putString("username", "brad");
+        editor.putBoolean("sound", true);
+        editor.putInt("stage", 7);
+        editor.commit();
+        Toast.makeText(this, "Save OK", Toast.LENGTH_SHORT).show();
     }
     public void test2(View view){
 
