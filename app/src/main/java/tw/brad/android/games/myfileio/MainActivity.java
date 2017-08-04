9a79c527d90d0cf74c9139e559fefc6cadf927fa
Brad <brad@brad.tw>
2017-08-04 20:48:33 +0800

    2017080402

--- app/src/main/java/tw/brad/android/games/myfileio/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/myfileio/MainActivity.java
@@ -2,6 +2,7 @@ package tw.brad.android.games.myfileio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void test1(View view){
+
+    }
+    public void test2(View view){
+
+    }
+    public void test3(View view){
+
+    }
+    public void test4(View view){
+
+    }
+    public void test5(View view){
+
+    }
+
 }
